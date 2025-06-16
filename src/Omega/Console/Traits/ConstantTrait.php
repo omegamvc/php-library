65a1ff4704815132a20e0f87f39246d6d7a0f7db
omegamvc <agisoftt@gmail.com>
2025-06-16 19:43:38 +0200

    Refactoring console

--- src/Omega/Console/Traits/ConstantTrait.php
+++ src/Omega/Console/Traits/ConstantTrait.php
@@ -1,18 +1,54 @@
 <?php
 
+/**
+ * Part of Omega - Console Package
+ * php version 8.3
+ *
+ * @link      https://omegamvc.github.io
+ * @author    Adriano Giovannini <agisoftt@gmail.com>
+ * @copyright Copyright (c) 2024 - 2025 Adriano Giovannini (https://omegamvc.github.io)
+ * @license   https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ * @version   2.0.0
+ */
+
 declare(strict_types=1);
 
 namespace Omega\Console\Traits;
 
 use function constant;
 
+/**
+ * Trait ConstantTrait
+ *
+ * Provides a utility method to dynamically resolve class constants by name.
+ * Useful for retrieving styling constants like color codes or predefined labels
+ * from string inputs at runtime.
+ *
+ * Example usage:
+ * ```php
+ * $value = self::getConstant('COLOR_RED');
+ * ```
+ * @category   Omega
+ * @package    Console
+ * @subpackage Traits
+ * @link       https://omegamvc.github.io
+ * @author     Adriano Giovannini <agisoftt@gmail.com>
+ * @copyright  Copyright (c) 2024 - 2025 Adriano Giovannini (https://omegamvc.github.io)
+ * @license    https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ * @version    2.0.0
+ */
 trait ConstantTrait
 {
     /**
-     * Get constant from string.
+     * Retrieve a class constant value by name.
      *
-     * @param string $name
-     * @return string|int Hex code
+     * Accepts the name of a constant as a string and returns its value.
+     * Particularly useful when mapping names to color codes or other fixed values.
+     *
+     * @param string $name The constant name (e.g., 'COLOR_RED').
+     * @return string|int The value of the constant (e.g., a color name or hex code).
+     *
+     * @throws \Error If the constant does not exist.
      */
     public static function getConstant(string $name): string|int
     {
