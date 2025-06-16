54fc2539b407aac610d9ce3ec35411234c6c10df
omegamvc <agisoftt@gmail.com>
2025-06-16 19:40:29 +0200

    Refactoring console

--- src/Omega/Console/Traits/AlertTrait.php
+++ src/Omega/Console/Traits/AlertTrait.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Part of Omega - Console Package
+ * php version 8.3
+ *
+ * @link      https://omegamvc.github.io
+ * @author    Adriano Giovannini <agisoftt@gmail.com>
+ * @copyright Copyright (c) 2024 - 2025 Adriano Giovannini (https://omegamvc.github.io)
+ * @license   https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ * @version   2.0.0
+ */
+
 declare(strict_types=1);
 
 namespace Omega\Console\Traits;
@@ -7,16 +18,35 @@ namespace Omega\Console\Traits;
 use Omega\Console\Style\Decorate;
 use Omega\Console\Style\Style;
 
+/**
+ * Trait AlertTrait
+ *
+ * Provides formatted alert messages (info, warning, fail, ok) using the Style builder.
+ * Supports customizable left margin padding for aligning messages visually in the terminal.
+ *
+ * Example usage:
+ * ```php
+ * $this->marginLeft(4)->warn('Something might go wrong.');
+ * ```
+ * @category   Omega
+ * @package    Console
+ * @subpackage Traits
+ * @link       https://omegamvc.github.io
+ * @author     Adriano Giovannini <agisoftt@gmail.com>
+ * @copyright  Copyright (c) 2024 - 2025 Adriano Giovannini (https://omegamvc.github.io)
+ * @license    https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ * @version    2.0.0
+ */
 trait AlertTrait
 {
-    /** @var int margin left */
+    /** @var int Number of spaces to prepend to each alert (left margin). */
     protected int $marginLeft = 0;
 
     /**
-     * Set margin left.
+     * Set the left margin (number of spaces) for alert messages.
      *
-     * @param int $marginLeft
-     * @return self
+     * @param int $marginLeft Number of spaces to prepend.
+     * @return static
      */
     public function marginLeft(int $marginLeft): static
     {
@@ -26,10 +56,10 @@ trait AlertTrait
     }
 
     /**
-     * Render alert info.
+     * Render a styled "info" alert message.
      *
-     * @param string $info
-     * @return Style
+     * @param string $info The message content.
+     * @return Style The rendered styled message.
      */
     public function info(string $info): Style
     {
@@ -42,15 +72,14 @@ trait AlertTrait
             ->bgBlue()
             ->push(' ')
             ->push($info)
-            ->newLines(2)
-        ;
+            ->newLines(2);
     }
 
     /**
-     * Render alert warning.
+     * Render a styled "warn" alert message.
      *
-     * @param string $warn
-     * @return Style
+     * @param string $warn The warning content.
+     * @return Style The rendered styled message.
      */
     public function warn(string $warn): Style
     {
@@ -63,15 +92,14 @@ trait AlertTrait
             ->bgYellow()
             ->push(' ')
             ->push($warn)
-            ->newLines(2)
-        ;
+            ->newLines(2);
     }
 
     /**
-     * Render alert fail.
+     * Render a styled "fail" alert message.
      *
-     * @param string $fail
-     * @return Style
+     * @param string $fail The failure content.
+     * @return Style The rendered styled message.
      */
     public function fail(string $fail): Style
     {
@@ -84,15 +112,14 @@ trait AlertTrait
             ->bgRed()
             ->push(' ')
             ->push($fail)
-            ->newLines(2)
-        ;
+            ->newLines(2);
     }
 
     /**
-     * Render alert ok (similar with success).
+     * Render a styled "ok" alert message, typically used to indicate success.
      *
-     * @param string $ok
-     * @return Style
+     * @param string $ok The success content.
+     * @return Style The rendered styled message.
      */
     public function ok(string $ok): Style
     {
@@ -105,7 +132,6 @@ trait AlertTrait
             ->bgGreen()
             ->push(' ')
             ->push($ok)
-            ->newLines(2)
-        ;
+            ->newLines(2);
     }
 }
